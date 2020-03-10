213af9f7df9cd03f610a9384907f67eed1ab4b34
vraj72 <virajtandel72@gmail.com>
2020-03-10 12:02:44 +0530

    index2.html added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path')
 const app = express()
 const register = require('./routes/register-server')
 const index = require('./routes/index-server')
+const index2 = require('./routes/index-server2')
 //var port = process.env.PORT || 8080
 var port = 8081
 app.use(cors())
@@ -16,6 +17,7 @@ app.set('view engine','pug')
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/register',register)
 app.use('/',index)
+app.use('/index2',index2)
 
 app.listen(port,()=>{
     console.log( `Server started on port ${port}`)
