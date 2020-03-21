7587a1128731e59812645bd034ba95a258d3c493
vraj72 <virajtandel72@gmail.com>
2020-03-21 13:14:07 +0530

    crud backend done

--- Project/routes/crud_admin.js
+++ Project/routes/crud_admin.js
@@ -15,18 +15,18 @@ router.post('/gettenderlist', function (req, res) {
 	db_1.default.query('SELECT * FROM `e_tender_details`  WHERE is_delete = 0', function (error, results, fields) {
 		if (error) {
 	      		console.log("error",error);
-	      		res.status(400);
-	      		// console.log("gettenderlist called0")	
+	      		res.sendStatus(400);
+	      		console.log("gettenderlist called0")	
 	     	}
 	     else{
 	       		if(results.length >0){
-	       			// console.log("gettenderlist called1")	
+	       			console.log("gettenderlist called1")	
 		  			res.send(results);
 
        			}
        			else{
-         			//User does not exist
-         			// console.log("gettenderlist called2")	
+         			
+         			console.log("gettenderlist called2")	
          			res.sendStatus(400);
        			}
       		}
@@ -51,14 +51,62 @@ router.post('/create_tender', function (req, res) {
 	db_1.default.query('INSERT INTO `e_tender_details` ( `et_title`, `et_tender_fee`, `et_tender_ref_no`, `et_tender_desc`, `et_last_date_apply`, `et_bidding_date`, `et_file_uri`, `is_delete`, `dept_id`) VALUES ( ?, ?, ?,?,?,?, ?, 0, ?);',[et_title ,et_tender_fee ,et_tender_ref_no ,et_tender_desc ,et_last_date_apply ,et_bidding_date ,et_file_uri ,dept_id] ,function (error, results, fields) {
 		if (error) {
 	      		console.log("error",error);
-	      		res.status(400);
+	      		res.sendStatus(400);
 	      		// console.log("gettenderlist called0")	
 	     	}
 	     else{
-	       		res.send(200);
+	       		res.sendStatus(200);
       		}
     	});
 });
+
+
+
+
+router.post('/update_tender', function (req, res) {
+
+	console.log("update tender called",req.body.et_title)
+	var et_id = req.body.et_id	
+	var et_title	 = req.body.et_title
+	var et_tender_fee	 = req.body.et_tender_fee
+	var et_tender_ref_no	 = req.body.et_tender_ref_no
+	var et_tender_desc	 = req.body.et_tender_desc
+	var et_last_date_apply	 = req.body.et_last_date_apply
+	var et_bidding_date	 = req.body.et_bidding_date
+	var et_file_uri	 = req.body.et_file_uri
+
+
+
+	db_1.default.query("UPDATE `e_tender_details` SET `et_title`= ? ,`et_tender_fee`= ? ,`et_tender_ref_no`= ?,`et_tender_desc`= ?,`et_last_date_apply`= ?,`et_bidding_date`= ? ,`et_file_uri`= ?  WHERE et_id = ? ;",[et_title ,et_tender_fee ,et_tender_ref_no ,et_tender_desc ,et_last_date_apply ,et_bidding_date ,et_file_uri ,et_id] ,function (error, results, fields) {
+		if (error) {
+	      		console.log("error",error);
+	      		res.sendStatus(400);
+	      		// console.log("gettenderlist called0")	
+	     	}
+	     else{
+	       		res.sendStatus(200);
+      		}
+    	});
+});
+
+router.post('/delete_tender', function (req, res) {
+
+	console.log("update tender called",req.body.et_id)
+	var et_id = req.body.et_id	
+	
+	db_1.default.query("UPDATE `e_tender_details` SET `is_delete`= 1  WHERE et_id = ? ;",[et_id] ,function (error, results, fields) {
+		if (error) {
+	      		console.log("error",error);
+	      		res.sendStatus(400);
+	      		// console.log("gettenderlist called0")	
+	     	}
+	     else{
+	       		res.sendStatus(200);
+      		}
+    	});
+});
+
+
 
 
 
