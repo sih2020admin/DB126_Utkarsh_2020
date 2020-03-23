9a323e8fc3b51e263a9d30dee8c9cf4772877630
winston <winstonsequeira@gmail.com>
2020-03-23 14:49:55 +0530

    ff

--- Project/app.ts
+++ Project/app.ts
@@ -5,9 +5,9 @@ import path from "path"
 import misc from './routes/misc'
 import payment from "./routes/payment-server"
 const app:Application = express()
-const login =require("./routes/login")
-const register=require("./routes/register-server")
-const tender_desc = require("./routes/tender_desc");
+var login = require("./routes/login");
+var register = require("./routes/register-server");
+var tender_desc = require("./routes/tender_desc");
 var crud_admin = require("./routes/crud_admin");
 
 //var port = process.env.PORT || 8080
@@ -17,13 +17,15 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views/user')));
+app.use(express.static(path.join(__dirname, 'views/admin')));
+
 
 app.use('/register',register.default)
 app.use("/misc",misc)
 app.use("/payment",payment)
 app.use('/', tender_desc.default);
-app.use('',login.default)
-app.use('/',crud_admin.default);
+app.use('', login.default);
+app.use('/', crud_admin.default);
 
 app.listen(port,()=>{
     console.log( `Server started on port ${port}`)
