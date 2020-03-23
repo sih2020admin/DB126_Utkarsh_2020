ddb17462bb30a6b7be4318744f23ae0e1c06c5dd
AFIF <46858532+Afif79@users.noreply.github.com>
2020-03-23 16:09:32 +0530

    crud updated sql updated

--- Project/public/javascript/crud_admin.js
+++ Project/public/javascript/crud_admin.js
@@ -266,7 +266,7 @@ var response;
 				  text: "You won't be able to revert this!",
 				  icon: 'warning',
 				  showCancelButton: true,
-				  confirmButtonColor: '#3085d6',
+				  confirmButtonColor: '#663EFD',
 				  cancelButtonColor: '#d33',
 				  confirmButtonText: 'Yes, delete it!'
 				}).then((result) => {
