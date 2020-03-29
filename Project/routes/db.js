ab9e518345406e56944ff07073880c08e2b107b3
winston <winstonsequeira@gmail.com>
2020-03-29 12:12:42 +0530

    modified db.js

--- Project/routes/db.js
+++ Project/routes/db.js
@@ -1,14 +1,70 @@
 "use strict";
+var __values = (this && this.__values) || function(o) {
+    var s = typeof Symbol === "function" && Symbol.iterator, m = s && o[s], i = 0;
+    if (m) return m.call(o);
+    if (o && typeof o.length === "number") return {
+        next: function () {
+            if (o && i >= o.length) o = void 0;
+            return { value: o && o[i++], done: !o };
+        }
+    };
+    throw new TypeError(s ? "Object is not iterable." : "Symbol.iterator is not defined.");
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var e_1, _a;
 Object.defineProperty(exports, "__esModule", { value: true });
+var os_1 = __importDefault(require("os"));
 var mysql_1 = __importDefault(require("mysql"));
-var connection = mysql_1.default.createConnection({
-    host: 'localhost',
-    user: 'winston',
-    password: 'Winston@99',
-    database: 'e_tender',
-    multipleStatements: true
-});
+var fs_1 = __importDefault(require("fs"));
+var connection;
+if (os_1.default.platform() === "linux" && os_1.default.hostname() === "ubuntu" && os_1.default.userInfo().username === "winston") {
+    var database, user, password;
+    console.log("hhhh");
+    var array = fs_1.default.readFileSync('/etc/mysql/my.cnf').toString().split("\n");
+    try {
+        for (var array_1 = __values(array), array_1_1 = array_1.next(); !array_1_1.done; array_1_1 = array_1.next()) {
+            var x = array_1_1.value;
+            if (x.match("database")) {
+                if (x.split("=")[1] != undefined) {
+                    database = x.split("=")[1].trim();
+                }
+            }
+            if (x.match("user")) {
+                if (x.split("=")[1] != undefined) {
+                    user = x.split("=")[1].trim();
+                }
+            }
+            if (x.match("password")) {
+                if (x.split("=")[1] != undefined) {
+                    password = x.split("=")[1].trim();
+                }
+            }
+        }
+    }
+    catch (e_1_1) { e_1 = { error: e_1_1 }; }
+    finally {
+        try {
+            if (array_1_1 && !array_1_1.done && (_a = array_1.return)) _a.call(array_1);
+        }
+        finally { if (e_1) throw e_1.error; }
+    }
+    connection = mysql_1.default.createConnection({
+        host: 'localhost',
+        user: user,
+        password: password,
+        database: database,
+        multipleStatements: true
+    });
+}
+else {
+    connection = mysql_1.default.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'e_tender',
+        multipleStatements: true
+    });
+}
 exports.default = connection;
--- Project/routes/db.ts
+++ Project/routes/db.ts
@@ -1,10 +1,48 @@
 import { Connection } from "mysql"
+import os from "os"
 import mysql from "mysql"
-var connection:Connection = mysql.createConnection({
-    host:'localhost',
-    user:'winston',
-    password:'Winston@99',
-    database:'e_tender',
-    multipleStatements:true
-})
+import fs from "fs"
+var connection:Connection;
+if (os.platform() === "linux" && os.hostname() === "ubuntu" && os.userInfo().username === "winston"){
+    var database,user,password
+    console.log("hhhh")
+    var array = fs.readFileSync('/etc/mysql/my.cnf').toString().split("\n");
+    for (var x of  array){
+        if (x.match("database")){
+            if(x.split("=")[1]!=undefined){
+                database = x.split("=")[1].trim()
+            } 
+        }
+        if (x.match("user")){
+            if(x.split("=")[1]!=undefined){
+                user = x.split("=")[1].trim()
+            } 
+        }
+        if (x.match("password")){
+            if(x.split("=")[1]!=undefined){
+                password = x.split("=")[1].trim()
+            } 
+        }
+
+    }
+
+    connection = mysql.createConnection({
+        host:'localhost',
+        user,
+        password,
+        database,
+        multipleStatements:true
+    })
+
+}
+else{
+     connection = mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'',
+        database:'e_tender',
+        multipleStatements:true
+    })
+}
+
 export default connection
