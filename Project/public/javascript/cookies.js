e31acd981b74471806da0ee24fb9754677d0ed8a
survir44 <survir44@gmail.com>
2020-04-15 11:03:19 +0000

    changes of e-sign ~saurabh

--- Project/public/javascript/cookies.js
+++ Project/public/javascript/cookies.js
@@ -1,9 +1,8 @@
 "use strict";
-var IP = "localhost";
 console.log("hello cookies");
 
 //if ur developing on server uncomment this IP
-// var IP="192.168.0.6"
+var IP="165.22.210.37"
 
 
 //if ur developing in ur pc uncomment this IP
