202cb2d0e6f6793ef594a412003473f98e493132
winston <winstonsequeira@gmail.com>
2020-04-29 13:40:54 +0530

    ff

--- Project/loader_modules/env-generate.js
+++ Project/loader_modules/env-generate.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = __importDefault(require("fs"));
+var fs_extra_1 = __importDefault(require("fs-extra"));
 var dotenv_1 = __importDefault(require("dotenv"));
 var debug = require('debug')('service:loader:env-generate');
 var ENV = /** @class */ (function () {
@@ -13,7 +13,7 @@ var ENV = /** @class */ (function () {
         this.production_env_path = production_env_path;
         this.development_folder_path = development_folder_path;
         this.development_env_path = development_env_path;
-        var loader_json = JSON.parse(fs_1.default.readFileSync(json_path).toString());
+        var loader_json = fs_extra_1.default.readJSONSync(json_path);
         for (var i in loader_json) {
             for (var j in loader_json[i]) {
                 this.env_object = this.env_object + (j.toUpperCase() + "=" + loader_json[i][j] + "\n");
@@ -21,32 +21,32 @@ var ENV = /** @class */ (function () {
         }
     }
     ENV.prototype.create = function () {
-        if (fs_1.default.existsSync('env')) {
+        if (fs_extra_1.default.existsSync('env')) {
             debug('Detected env folder in current directory');
-            if (fs_1.default.existsSync(this.production_folder_path) === false && fs_1.default.existsSync(this.development_folder_path) === false) {
+            if (fs_extra_1.default.existsSync(this.production_folder_path) === false && fs_extra_1.default.existsSync(this.development_folder_path) === false) {
                 debug('Could not find production and development folder in env folder\nCreating them');
                 this.create_both();
             }
             {
-                if (fs_1.default.existsSync(this.production_folder_path)) {
-                    if (fs_1.default.existsSync(this.production_env_path)) {
+                if (fs_extra_1.default.existsSync(this.production_folder_path)) {
+                    if (fs_extra_1.default.existsSync(this.production_env_path)) {
                         debug('.env file present in production folder');
                     }
                     else {
                         debug('Creating .env file in production folder');
-                        fs_1.default.writeFileSync(this.production_env_path, '');
+                        fs_extra_1.default.writeFileSync(this.production_env_path, '');
                     }
                 }
                 else {
                     this.create_prod();
                 }
-                if (fs_1.default.existsSync(this.development_folder_path)) {
-                    if (fs_1.default.existsSync(this.development_env_path)) {
+                if (fs_extra_1.default.existsSync(this.development_folder_path)) {
+                    if (fs_extra_1.default.existsSync(this.development_env_path)) {
                         debug('.env file present in development folder');
                     }
                     else {
                         debug('Creating .env file in development folder');
-                        fs_1.default.writeFileSync(this.development_env_path, '');
+                        fs_extra_1.default.writeFileSync(this.development_env_path, '');
                     }
                 }
                 else {
@@ -56,32 +56,32 @@ var ENV = /** @class */ (function () {
         }
         else {
             debug('Creating env folder');
-            fs_1.default.mkdirSync('env');
+            fs_extra_1.default.mkdirSync('env');
             this.create_both();
         }
     };
     ENV.prototype.create_dev = function () {
         debug("Couldn't find development folder in env folder");
-        fs_1.default.mkdirSync(this.development_folder_path);
+        fs_extra_1.default.mkdirSync(this.development_folder_path);
         debug('Creating .env file in development folder');
-        fs_1.default.writeFileSync(this.development_env_path, '');
+        fs_extra_1.default.writeFileSync(this.development_env_path, '');
     };
     ENV.prototype.create_prod = function () {
         debug("Couldn't find production folder in env folder");
-        fs_1.default.mkdirSync(this.production_folder_path);
+        fs_extra_1.default.mkdirSync(this.production_folder_path);
         debug('Creating .env file in production folder');
-        fs_1.default.writeFileSync(this.production_env_path, '');
+        fs_extra_1.default.writeFileSync(this.production_env_path, '');
     };
     ENV.prototype.create_both = function () {
         debug('Create both folder function called');
         debug('Creating production folder in env folder');
-        fs_1.default.mkdirSync(this.production_folder_path);
+        fs_extra_1.default.mkdirSync(this.production_folder_path);
         debug('Creating .env file in production folder');
-        fs_1.default.writeFileSync(this.production_env_path, '');
+        fs_extra_1.default.writeFileSync(this.production_env_path, '');
         debug('Creating development folder in env folder');
-        fs_1.default.mkdirSync(this.development_folder_path);
+        fs_extra_1.default.mkdirSync(this.development_folder_path);
         debug('Creating .env file in development folder');
-        fs_1.default.writeFileSync(this.development_env_path, '');
+        fs_extra_1.default.writeFileSync(this.development_env_path, '');
     };
     ENV.prototype.load = function (server) {
         if (server === true) {
@@ -99,10 +99,10 @@ var ENV = /** @class */ (function () {
     };
     ENV.prototype.write = function (server, ipv4_address) {
         if (server === true) {
-            fs_1.default.writeFileSync(this.production_env_path, this.env_object + ("ADDRESS=" + ipv4_address));
+            fs_extra_1.default.writeFileSync(this.production_env_path, this.env_object + ("ADDRESS=" + ipv4_address));
         }
         else {
-            fs_1.default.writeFileSync(this.development_env_path, this.env_object + ("ADDRESS=" + ipv4_address));
+            fs_extra_1.default.writeFileSync(this.development_env_path, this.env_object + ("ADDRESS=" + ipv4_address));
         }
     };
     return ENV;
