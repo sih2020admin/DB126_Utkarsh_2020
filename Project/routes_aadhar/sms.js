32f3c121fe825dc300ae041eada7f51e33b3947f
survir44 <survir44@gmail.com>
2020-05-07 06:11:56 +0530

    E-sign SMS added

---
+++ Project/routes_aadhar/sms.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var router = express.Router();
+const assert = require('assert');
+
+//MySql Connection
+var mysql = require('mysql')
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: "root",
+    password: "",
+    database: 'aadharDB',
+});
+con.connect(function (err) {
+    if (err){
+	console.log("Not Connected To Mysql!!!");
+    }
+    console.log('Connected To Mysql !!!');
+});
+
+//Email
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASS,
+    },
+});
+
+//SMS
+const accountSid = 'ACb6efbe87d9f5fca5440bbc802d064fe9'; 
+const authToken = '4664d67664a57f3b6b6fd4055ed2f825'; 
+const client = require('twilio')(accountSid, authToken); 
+
+//OTP Generation
+function getRandomInt() {
+    min = Math.ceil(100000);
+    max = Math.floor(999999);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+router.post('/send',(req,res)=>{
+    var usrn = Number(req.body.aadharno);
+    console.log("verify called "+usrn);
+
+    con.query('SELECT * FROM aadhar_details WHERE aadharno= ? ',[usrn],function (error,results){
+        if (error) {
+            console.log("error");
+            res.sendStatus(400);
+        } else {
+            if (results.length > 0) {
+                //console.log("number",results[0].phone_number," email",results[0].email);
+                const otp = getRandomInt();
+                
+                //SMS sending
+                var smsOptions = {
+                    from: '+17622142266',       
+                    to: '+91' + results[0].phone_number, 
+                    body: 'Aadhar OTP  for E-signing is ' + otp + ' and is valid for 5 minutes.', 
+                }
+                client.messages.create(smsOptions,function(error,message){
+                    if(error){
+                        console.log("error");
+                        //res.sendStatus(400);
+                    }else{
+                        console.log("SMS sent:" + message.sid);
+                    }
+                });
+
+                // Database part
+                var dt = new Date(Date.now() + 300000);
+                dt = dt.toISOString().slice(0, 19).replace('T', ' ');
+                //console.log(dt, new Date(Date.now()))
+                con.query('INSERT INTO `OTP` (`aadharno`, `otp`, `validtill`, `isUsed`, `reference_id`) VALUES (?,?,?, 0 , 1)', [usrn, otp, dt], function (error, results, fields) {
+                    if (error) {
+                        //console.log("error: otp db ",error);
+                        res.sendStatus(400);
+                    } else {
+                        //console.log("session ",usrn,otp);
+                        console.log('Aadhar OTP:', otp);
+                        //console.log("sucess otp db ");
+                        res.sendStatus(200);
+                    }
+                });
+            } else {
+                //console.log("error");
+                res.sendStatus(400);
+            }
+        }
+    });
+});
+
+
+
+module.exports = router
