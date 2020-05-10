a0b4e8d948b508ae8b767884908437154e76bc31
vraj72 <virajtandel72@gmail.com>
2020-05-10 12:50:13 +0530

    crud bugs fixed

--- Project/public/javascript/2crud_admin.js
+++ Project/public/javascript/2crud_admin.js
@@ -127,7 +127,7 @@ jQuery('document').ready(function(){
                          res=JSON.parse(res)
                          file1_uri=res.filename
                          if(file2_uri != null && file1_uri != null){
-                         	document.getElementById("get_link").style.display = "block";
+                         	// document.getElementById("get_link").style.display = "block";
                          }
                         }
                     });
@@ -187,7 +187,7 @@ jQuery('document').ready(function(){
                          res=JSON.parse(res)
                          file2_uri=res.filename
                          if(file2_uri != null && file1_uri != null){
-                         	document.getElementById("get_link").style.display = "block";
+                         	// document.getElementById("get_link").style.display = "block";
                          }
                         }
                     });
@@ -570,7 +570,12 @@ jQuery('document').ready(function(){
 					alert("Bid opening date must be after closing date")
 
 					return false
-				}else{
+				}
+				else if(zip_link==null){
+					alert("Upload documents and Generate Link")
+					return false
+				}
+				else{
 
 					var xhr = new XMLHttpRequest();
 					// xhr.withCredentials = true;
@@ -609,6 +614,10 @@ jQuery('document').ready(function(){
 
 function get_link() {
 	// body...
+	if(file1_uri==null && file2_uri==null){
+		alert("Upload File first to generate link")
+		return false
+	}
 	var data = JSON.stringify({"file1_uri":file1_uri,"file2_uri":file2_uri});
 
 	var xhr = new XMLHttpRequest();
@@ -635,23 +644,26 @@ function select_files(value) {
 	// body...
 	if(value == '0'){
 		zip_link = "uploads/sample.zip"
+		document.getElementById("get_link").style.display = "none";
 		document.getElementById("create_t").style.display = "block";
+		document.getElementById("div_f1").style.display = "none";
+		document.getElementById("div_f2").style.display = "none";
 	}else if(value == '1'){
 		file2_uri=" "
 		file1_uri=null;
-		document.getElementById("create_t").style.display = "none";
+		// document.getElementById("create_t").style.display = "none";
 		document.getElementById("div_f1").style.display = "block";
 		document.getElementById("div_f2").style.display = "none";
 	}else if(value =='2'){
 		file1_uri=' '
 		file2_uri=null;
-		document.getElementById("create_t").style.display = "none";
+		// document.getElementById("create_t").style.display = "none";
 		document.getElementById("div_f1").style.display = "none";
 		document.getElementById("div_f2").style.display = "block";
 	}else if(value == '3'){
 		file1_uri=null;
 		file2_uri=null;
-		document.getElementById("create_t").style.display = "none";
+		// document.getElementById("create_t").style.display = "none";
 		document.getElementById("div_f1").style.display = "block";
 		document.getElementById("div_f2").style.display = "block";
 	}
