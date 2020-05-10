56e07a4b9f4d2303d80c806d77cd22f5a2f590e4
survir44 <survir44@gmail.com>
2020-05-10 12:30:35 +0530

    Payment page active

--- Project/routes/tender_desc.js
+++ Project/routes/tender_desc.js
@@ -33,30 +33,6 @@ router.post('/tender_desc', function (req, res) {
     	});
 });
 
-
-router.post('/filled_tender_desc', function (req, res) {
-
-    	var et_id= req.body.et_id;
-    	var etd_id= req.body.etd_id;
-    	console.log("tender desc called "+etd_id) 
-	
-				
-	db_1.default.query('SELECT * FROM  e_tender_details AS e INNER JOIN e_tender_vendor AS etv ON e.et_id = etv.et_id  WHERE etv.etd_id = ?',[etd_id], function (error, results, fields) {
-		if (error) {
-	      		//console.log("error");
-	      		res.sendStatus(400);
-	     	}else{
-	       		if(results.length >0){
-	       			res.send(results[0]);
-       			}
-       			else{
-         			//does not exists
-         			res.sendStatus(400);
-       			}
-      		}
-    	});
-});
-
 router.post('/get_etd_id', function (req, res) {
 
 	var et_id= req.body.et_id;
@@ -106,9 +82,7 @@ router.post('/view', function (req, res) {
 						});
 					}
 				});
-			}
-
-				
+			}	
 		});
 });
 
