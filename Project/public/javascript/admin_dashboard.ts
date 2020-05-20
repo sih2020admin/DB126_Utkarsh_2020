2b96ef536a28649efbf41cd0dbf2b7c4f32e5b69
winston <winstonsequeira@gmail.com>
2020-05-20 16:08:53 +0530

    f

--- Project/public/javascript/admin_dashboard.ts
+++
@@ -1,9 +0,0 @@
-$('#approved_button').click(()=>{
-    $('.details').hide()
-    $('.approved_tenders').fadeTo('fast', 1)
-})
-
-$('#profile_button').click(()=>{
-    $('.approved_tenders').hide()    
-    $('.details').fadeTo('fast', 1)
-})
