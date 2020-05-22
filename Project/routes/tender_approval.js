41f22cd3a4368a308eae1b87be125d66296aae6e
meetsanket24@gmail.com <meetsanket24@gmail.com>
2020-05-22 19:43:52 +0530

    check file digi

--- Project/routes/tender_approval.js
+++ Project/routes/tender_approval.js
@@ -193,7 +193,7 @@ function check_file(vcd_id, tech_furi, boq_furi, is_tech, results) {
 function file_status_digi(results) {
 
 	for (var i = 0; i < results.length; i++) {
-		console.log(results[i]);
+		console.log(results[i], results[i].furi1, results[i].furi2);
 		var vcd_id_ = results[i].vcd_id;
 
 		//console.log("results", results, results.length);
