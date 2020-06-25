4530028e73b00c35eaced0fa604be218ab4cd32f
vraj72 <virajtandel72@gmail.com>
2020-06-25 15:42:52 +0530

    filter date done

--- Project/public/javascript/v3_see_tender.js
+++ Project/public/javascript/v3_see_tender.js
@@ -167,7 +167,7 @@ function filterData() {
     filter_categories.push($('#fees').val().toString())
     filtered_result = filter_department(filter_categories[0])
     filtered_result = filter_fees(filter_categories[2], filtered_result)
-    //filtered_result = filter_closing_date(filter_categories[1], filtered_result)
+    filtered_result = filter_closing_date(filter_categories[1], filtered_result)
     render_filtered_results(filtered_result)
 }
 function modify_date(date) {
@@ -208,7 +208,7 @@ function filter_closing_date(dates, result){
     console.log("dates",dates,result)
     if (result.length !== 0) {
         if(dates[0]==="" && dates[1]==="" ){
-            // console.log("1 ")
+            console.log("1 ")
             return result;
         }
         else if(dates[1]===""){ 
@@ -234,7 +234,7 @@ function filter_closing_date(dates, result){
 
         }
         else {
-            // console.log("4 ")
+            console.log("4 ")
             for (let i = 0; i < result.length; i++) {
                 // console.log((Date.parse(result[i].et_last_date_apply.slice(0,10)) <=Date.parse(dates[1]) && Date.parse(result[i].et_last_date_apply.slice(0,10)) >=Date.parse(dates[0])) , Date.parse(result[i].et_last_date_apply.slice(0,10)) ,Date.parse(dates[0]) ,Date.parse(dates[1]))
                 if(Date.parse(result[i].et_last_date_apply.slice(0,10)) <=Date.parse(dates[1]) && Date.parse(result[i].et_last_date_apply.slice(0,10)) >=Date.parse(dates[0])){
