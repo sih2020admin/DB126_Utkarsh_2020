2dc8212fae5c7bac5b6baa25c935e1a58d282a83
survir44 <survir44@gmail.com>
2020-07-04 03:02:06 +0530

    admin reference removed

--- Project/routes/crud_admin.js
+++ Project/routes/crud_admin.js
@@ -119,7 +119,7 @@ router.post('/create_tender', function (req, res) {
 	console.log("craete tender called",req.body.et_title)	
 	var et_title	 = req.body.et_title
 	var et_tender_fee	 = req.body.et_tender_fee
-	var et_tender_ref_no	 = req.body.et_tender_ref_no
+	// var et_tender_ref_no	 = req.body.et_tender_ref_no
 	var et_tender_desc	 = req.body.et_tender_desc
 	var et_last_date_apply	 = req.body.et_last_date_apply
 	var et_bidding_date	 = req.body.et_bidding_date
@@ -129,21 +129,33 @@ router.post('/create_tender', function (req, res) {
     var dept_id = req.signedCookies.ad_dept_id_e;
     // var ad_org_id = req.signedCookies.ad_org_id_e;
 
-
-	db_1.default.query('INSERT INTO `e_tender_details` ( `et_title`, `et_tender_fee`, `et_tender_ref_no`, `et_tender_desc`, `et_last_date_apply`, `et_bidding_date`, `et_file_uri`, `is_delete`, `dept_id`) VALUES ( ?, ?, ?,?,?,?, ?, 0, ?);',[et_title ,et_tender_fee ,et_tender_ref_no ,et_tender_desc ,et_last_date_apply ,et_bidding_date ,et_file_uri ,dept_id] ,function (error, results, fields) {
-		if (error) {
-	      		console.log("error",error);
-	      		if (error.code == "ER_DUP_ENTRY") {
-	      			res.status(400)
-	      			res.send("Duplicate entry")
-	      		}
-	      		else res.sendStatus(400);
-	      		// console.log("gettenderlist called0")	
-	     	}
-	     else{
-	       		res.sendStatus(200);
-      		}
-    	});
+	db_1.default.query('SELECT dept_name,(SELECT max(et_id) FROM e_tender_details) as et_id FROM department WHERE dept_id=?',[dept_id],function(err,result){
+		if(err){
+			res.sendStatus(400);
+			console.log("Create Tender Department Fetching Error");
+		}
+		else{
+			const todaysDate = new Date()
+			const currentYear = todaysDate.getFullYear()
+			var et_id=result[0].et_id;
+			et_id+=1;
+			var ref_no= currentYear + '/' + et_id+ '/' + result[0].dept_name + et_id;
+			db_1.default.query('INSERT INTO `e_tender_details` ( `et_title`, `et_tender_fee`, `et_tender_ref_no`, `et_tender_desc`, `et_last_date_apply`, `et_bidding_date`, `et_file_uri`, `is_delete`, `dept_id`) VALUES ( ?, ?, ?,?,?,?, ?, 0, ?);',[et_title ,et_tender_fee ,ref_no ,et_tender_desc ,et_last_date_apply ,et_bidding_date ,et_file_uri ,dept_id] ,function (error, results, fields) {
+				if (error) {
+					console.log("error",error);
+					if (error.code == "ER_DUP_ENTRY") {
+						res.status(400)
+						res.send("Duplicate entry")
+					}
+					else res.sendStatus(400);
+					// console.log("gettenderlist called0")	
+				}
+				else{
+					res.sendStatus(200);
+				}
+			});
+		}
+	});	
 });
 
 
