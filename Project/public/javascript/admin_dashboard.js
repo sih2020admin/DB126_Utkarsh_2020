a42b522c01b849a3f14cbe6fe1ff3726be8857a5
AFIF <46858532+Afif79@users.noreply.github.com>
2020-07-11 16:01:43 +0530

    admin dashboard button css applied again

--- Project/public/javascript/admin_dashboard.js
+++ Project/public/javascript/admin_dashboard.js
@@ -2,8 +2,12 @@
 $('#approved_button').click(function () {
     $('.details').hide();
     $('.approved_tenders').fadeTo('fast', 1);
+    document.getElementById("profile_button").classList.remove("active");
+    document.getElementById("approved_button").classList.add("active");
 });
 $('#profile_button').click(function () {
     $('.approved_tenders').hide();
     $('.details').fadeTo('fast', 1);
+    document.getElementById("approved_button").classList.remove("active");
+    document.getElementById("profile_button").classList.add("active");
 });
