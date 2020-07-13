2d5c4b84908e59a1df255c4e05812e0ab3c719cc
vraj72 <virajtandel72@gmail.com>
2020-07-13 19:51:03 +0530

    vcd id took from cookie

--- Project/routes/render-user.js
+++ Project/routes/render-user.js
@@ -105,11 +105,13 @@ router.get('/tender/upload-documents', function (request, response) {
     });
 });
 router.get('/tender/confirmation', function (request, response) {
+    console.log("vcd in confirmation page 1",request.signedCookies.vcd_id_e);
     var user = user_1.isUser(request);
     Promise.all([user_1.getUserUsername(request), tender_1.confirmedTenderDetails(request)])
         .then(function (results) {
-        results[1][2][0]['vcd'] = request.signedCookies.vcd_id_e;
-        console.log("vcd in confirmation page",results[1][2][0]['vcd'],request.signedCookies.vcd_id_e);
+        // results[1][2][0]['vcd'] = request.signedCookies.vcd_id_e;
+        console.log("vcd in confirmation page 2",request.signedCookies.vcd_id_e);
+        console.log("vcd in confirmation page 3",results);
         response.render('user/tender_confirmation', { layout: false, user: user, username: results[0], tender_details: results[1][0][0], personal_details: results[1][1][0], payment_details: results[1][2][0], bid_amt: results[1][3][0],vcd: request.signedCookies.vcd_id_e });
     })
         .catch(function (error) {
