33c0f12777bb478f5827aacb4225cd01e54ba4d8
winston <winstonsequeira@gmail.com>
2020-07-16 09:30:44 +0530

    modified redirectiion

--- Project/miscellaneous/middleware/user/redirect.js
+++ Project/miscellaneous/middleware/user/redirect.js
@@ -1,7 +1,14 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.redirectToLoginPage = exports.redirectToProfilePage = void 0;
+const toml_1 = __importDefault(require("@iarna/toml"));
+const fs_1 = __importDefault(require("fs"));
 const debug = require('debug')('middleware:redirect');
+// @ts-ignore
+var exception = new RegExp('\/' + toml_1.default.parse(fs_1.default.readFileSync('configuration/middleware.toml').toString())['Exceptions']['User']['routes'].join('|\/'));
 function redirectToProfilePage(request, response, next) {
     if (request.url.match('/admin') === null) {
         if (request.url.match(/\/login|\/register/)) {
@@ -19,7 +26,7 @@ function redirectToLoginPage(request, response, next) {
     /* console.table({XHR:request.xhr,accepts:request.accepts(['html','json']),value:request.headers["x-requested-with"]}) */
     if (request.url.match('/admin') === null) {
         if (request.url !== '/') {
-            if (request.url.match(/\/login|\/register|\/help|\/get_files|\/faq|\/feedback/) === null && request.method === 'GET') {
+            if (request.url.match(exception) === null && request.method === 'GET') {
                 if (request.signedCookies['vcd_id_e'] === undefined || request.signedCookies['vd_id_e'] === undefined) {
                     debug('Cookies have been deleted or modified');
                     debug('Redirecting to Login page');
