e1e77523cffbd03209fc869cc0e44d85bd820905
vraj72 <virajtandel72@gmail.com>
2020-07-27 18:12:51 +0530

    crypto removed

--- Project/routes/register-server.js
+++ Project/routes/register-server.js
@@ -9,7 +9,7 @@ const  crypto = require('crypto')
 var express_1 = __importDefault(require('express'))
 var db_1 = __importDefault(require('./db'))
 var router = express_1.default.Router()
-var crypto = require('crypto')
+// var crypto = require('crypto')
 
 var nodemailer = require('nodemailer')
 var transporter = nodemailer.createTransport({
